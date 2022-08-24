e77e4d9d3b1b9f72f288011b29a92c8c1e3ac964
hansunglee <hansunglee@sunssc.com>
2022-08-24 17:11:16 -0400

    Parameter vs Object

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,32 @@
-function Movie({ title, coverImg, summary, genres }) {
+// -------------------------------------------------
+// 1) Each Parameter
+// -------------------------------------------------
+// function Movie({ title, coverImg, summary, genres }) {
+//   return (
+//     <div>
+//       <h2>{title}</h2>
+//       <img alt={title} src={coverImg}></img>
+//       <p>{summary}</p>
+//       <ul>
+//         {genres.map((genre, index) => (
+//           <li key={index}>{genre}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// -------------------------------------------------
+// 2) Whole Object
+// -------------------------------------------------
+function Movie(props) {
   return (
     <div>
-      <h2>{title}</h2>
-      <img alt={title} src={coverImg}></img>
-      <p>{summary}</p>
+      <h2>{props.movie.title}</h2>
+      <img alt={props.movie.title} src={props.movie.medium_cover_image}></img>
+      <p>{props.movie.summary}</p>
       <ul>
-        {genres.map((genre, index) => (
+        {props.movie.genres.map((genre, index) => (
           <li key={index}>{genre}</li>
         ))}
       </ul>
