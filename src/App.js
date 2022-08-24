df3a8d3320cb3759715fa06b89dc47ecc1c33e9a
hansunglee <hansunglee@sunssc.com>
2022-08-24 16:11:12 -0400

    Movie Fetch and Display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,65 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div className="App">hi</div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  // ---------------------------------------------------------------------------------
+  // Way [1]
+  // ---------------------------------------------------------------------------------
+  // useEffect(() => {
+  //   fetch(
+  //     "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setMovies(json.data.movies);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  // ---------------------------------------------------------------------------------
+  // Way [2]
+  // ---------------------------------------------------------------------------------
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+    );
+    const json = await response.json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  // ---------------------------------------------------------------------------------
+
+  console.log(movies);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        movies.map((movie) => (
+          <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <img alt={movie.title} src={movie.medium_cover_image}></img>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default App;
